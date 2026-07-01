a76c97b3a508e73f03c62b86d48864001b4e54e4
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-07-01 19:17:50 +0330

    searchBox

--- src/components/elements/Search.tsx
+++ src/components/elements/Search.tsx
@@ -1,12 +1,52 @@
 import { Button } from "../ui/button";
 import { FiSearch } from "react-icons/fi";
 
+import styles from "@/components/elements/styles/search/route.module.css";
+import SearchBox from "../modules/SearchBox";
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+
+const SearchDropDown = styled.div<{ $isOpen: boolean }>`
+  opacity: ${(props) => (props.$isOpen ? 1 : 0)};
+  transform: translateY(${(props) => (props.$isOpen ? "0" : "-100%")});
+  transition: opacity 0.4s ease;
+  backdrop-filter: blur(0.6px);
+  display: flex;
+  margin-top: ${(props) => (props.$isOpen ? "4.6rem" : "-100%")};
+
+  justify-content: center;
+  height: 100vh;
+  position: fixed;
+  inset: 0;
+  z-index: 10;
+  overflow: hidden;
+`;
+
 const Search = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const clickHandler = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const overFlowHandler = () => {
+    document.body.style.overflow = isOpen ? "hidden" : "auto";
+  };
+
+  useEffect(() => {
+    overFlowHandler();
+  }, [isOpen]);
+
   return (
-    <Button className="bg-[#484848] w-50 justify-start  p-2 flex gap-2 items-center rounded-md cursor-pointer ">
-      <FiSearch />
-      <span className="text-[0.8rem]">Search</span>
-    </Button>
+    <>
+      <Button className={styles.searchButton} onClick={clickHandler}>
+        <FiSearch />
+        <span className="text-[0.8rem]">Search</span>
+      </Button>
+      <SearchDropDown $isOpen={isOpen}>
+        <SearchBox setIsOpen={setIsOpen} />
+      </SearchDropDown>
+    </>
   );
 };
 
--- src/components/modules/SearchBox.tsx
+++ src/components/modules/SearchBox.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { type Dispatch, type SetStateAction } from "react";
+import { FiSearch } from "react-icons/fi";
+import { IoClose } from "react-icons/io5";
+import styles from "@/components/modules/css/searchBox/route.module.css";
 
-const SearchBox = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+interface SearchBoxProps {
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const SearchBox = ({ setIsOpen }: SearchBoxProps) => {
+  return (
+    <section className={styles.searchBox}>
+      <div className={styles.searchBox__container}>
+        <FiSearch />
+
+        <input
+          type={"text"}
+          className={styles.searchBox___searchInput}
+          placeholder="Search coin, pair, contract address, exchange, or post"
+        />
+      </div>
+
+      <IoClose className="cursor-pointer ml-auto" onClick={() => setIsOpen(false)} />
+    </section>
+  );
 };
 
 export default SearchBox;
