7cc4ea529d86b775bfc4fca97c0399a0d55a901e
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-08-12 22:31:18 +0330

    router changes

--- src/components/router/RouterPage.tsx
+++ src/components/router/RouterPage.tsx
@@ -1,8 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "@/components/templates/Home";
 
-import { lazy, Suspense } from "react";
-import Loader from "@/components/loader/Loader";
+import { lazy } from "react";
 
 const CoinDetail = lazy(() => import("@/components/templates/CoinDetail"));
 const Favorite = lazy(() => import("@/components/templates/Favorite"));
@@ -12,16 +11,9 @@ const RouterPage = () => {
     <Routes>
       <Route element={<Home />} path="/" />
 
-      <Suspense
-        fallback={
-          <div className="mx-auto">
-            <Loader />
-          </div>
-        }
-      >
-        <Route element={<CoinDetail />} path=":coinId" />
-        <Route element={<Favorite />} path="/favorite" />
-      </Suspense>
+      <Route element={<CoinDetail />} path=":coinId" />
+      <Route element={<Favorite />} path="/favorite" />
+
       <Route
         path="*"
         element={
@@ -33,33 +25,3 @@ const RouterPage = () => {
 };
 
 export default RouterPage;
-
-// import { lazy, Suspense } from "react";
-// import { Route, Routes } from "react-router-dom";
-
-// const Home = lazy(() => import("@/components/templates/Home"));
-// const CoinDetail = lazy(() => import("@/components/templates/CoinDetail"));
-// const Favorite = lazy(() => import("@/components/templates/Favorite"));
-
-// const RouterPage = () => {
-//   return (
-//     <Suspense fallback={<div>Loading...</div>}>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-
-//         <Route path=":coinId" element={<CoinDetail />} />
-
-//         <Route path="/favorite" element={<Favorite />} />
-
-//         <Route
-//           path="*"
-//           element={
-//             <h2 className="w-full text-center mt-12">something went wrong</h2>
-//           }
-//         />
-//       </Routes>
-//     </Suspense>
-//   );
-// };
-
-// export default RouterPage;
